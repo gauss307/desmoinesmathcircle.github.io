7bf36cb25d1f52adddcd19d3afd496caa8ac6a01
gauss307 <maxsquid567@gmail.com>
2024-08-03 20:05:26 -0500

    third

---
+++ dmmc.js
@@ -0,0 +1,29 @@
+const box1 = document.querySelector(".body3-box1");
+
+box1.addEventListener("mouseenter", () => {
+    box1.style.opacity = "0.5";
+});
+
+box1.addEventListener("mouseleave", () => {
+    box1.style.opacity = "1";
+});
+
+const box2 = document.querySelector(".body3-box2");
+
+box2.addEventListener("mouseenter", () => {
+    box2.style.opacity = "0.5";
+});
+
+box2.addEventListener("mouseleave", () => {
+    box2.style.opacity = "1";
+});
+
+const box3 = document.querySelector(".body3-box3");
+
+box3.addEventListener("mouseenter", () => {
+    box3.style.opacity = "0.5";
+});
+
+box3.addEventListener("mouseleave", () => {
+    box3.style.opacity = "1";
+});
