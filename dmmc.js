0eb591b0726c160f820a158193158f53f26ac8c7
gauss307 <maxsquid567@gmail.com>
2024-08-04 10:08:16 -0500

    fifth

--- dmmc.js
+++ dmmc.js
@@ -27,3 +27,18 @@ box3.addEventListener("mouseenter", () => {
 box3.addEventListener("mouseleave", () => {
     box3.style.opacity = "1";
 });
+
+/*promysInterviews*/
+function inter(evt, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("inter");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
+  }
